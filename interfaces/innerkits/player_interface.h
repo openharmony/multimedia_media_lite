ae7657711818f12c001191fe0227b7f348303e1d
ancimoon <huchang4@huawei.com>
2021-10-18 02:03:00 +0000

    !3 format gn files and code check fix
    Signed-off-by: Hu Chang <huchang4@huawei.com>

--- interfaces/innerkits/player_interface.h
+++ interfaces/innerkits/player_interface.h
@@ -25,33 +25,33 @@ namespace OHOS {
 namespace Media {
 class PlayerInterface {
 public:
-  virtual ~PlayerInterface() = default;
-  virtual int32_t SetSource(const Source &source) = 0;
-  virtual int32_t Prepare() = 0;
-  virtual int32_t Play() = 0;
-  virtual bool IsPlaying() = 0;
-  virtual int32_t Pause() = 0;
-  virtual int32_t Stop() = 0;
-  virtual int32_t Rewind(int64_t mSeconds, int32_t mode) = 0;
-  virtual int32_t SetVolume(float leftVolume, float rightVolume) = 0;
-  virtual int32_t SetSurface(Surface *surface) = 0;
-  virtual bool IsSingleLooping() = 0;
-  virtual int32_t GetPlayerState(int32_t &state) = 0;
-  virtual int32_t GetCurrentPosition(int64_t &currentPosition) = 0;
-  virtual int32_t GetDuration(int64_t &duration) = 0;
-  virtual int32_t GetVideoWidth(int32_t &videoWidth) = 0;
-  virtual int32_t GetVideoHeight(int32_t &videoHeight) = 0;
-  virtual int32_t SetPlaybackSpeed(float speed) = 0;
-  virtual int32_t GetPlaybackSpeed(float &speed) = 0;
-  virtual int32_t SetAudioStreamType(int32_t type) = 0;
-  virtual void GetAudioStreamType(int32_t &type) = 0;
-  virtual int32_t Reset(void) = 0;
-  virtual int32_t Release() = 0;
-  virtual int32_t SetLoop(bool loop) = 0;
-  virtual void SetPlayerCallback(const std::shared_ptr<PlayerCallback> &cb) = 0;
-  virtual int32_t Init(void) = 0;
-  virtual int32_t DeInit(void) = 0;
-  virtual int32_t SetParameter(const Format &params) = 0;
+    virtual ~PlayerInterface() = default;
+    virtual int32_t SetSource(const Source &source) = 0;
+    virtual int32_t Prepare() = 0;
+    virtual int32_t Play() = 0;
+    virtual bool IsPlaying() = 0;
+    virtual int32_t Pause() = 0;
+    virtual int32_t Stop() = 0;
+    virtual int32_t Rewind(int64_t mSeconds, int32_t mode) = 0;
+    virtual int32_t SetVolume(float leftVolume, float rightVolume) = 0;
+    virtual int32_t SetSurface(Surface *surface) = 0;
+    virtual bool IsSingleLooping() = 0;
+    virtual int32_t GetPlayerState(int32_t &state) = 0;
+    virtual int32_t GetCurrentPosition(int64_t &currentPosition) = 0;
+    virtual int32_t GetDuration(int64_t &duration) = 0;
+    virtual int32_t GetVideoWidth(int32_t &videoWidth) = 0;
+    virtual int32_t GetVideoHeight(int32_t &videoHeight) = 0;
+    virtual int32_t SetPlaybackSpeed(float speed) = 0;
+    virtual int32_t GetPlaybackSpeed(float &speed) = 0;
+    virtual int32_t SetAudioStreamType(int32_t type) = 0;
+    virtual void GetAudioStreamType(int32_t &type) = 0;
+    virtual int32_t Reset(void) = 0;
+    virtual int32_t Release() = 0;
+    virtual int32_t SetLoop(bool loop) = 0;
+    virtual void SetPlayerCallback(const std::shared_ptr<PlayerCallback> &cb) = 0;
+    virtual int32_t Init(void) = 0;
+    virtual int32_t DeInit(void) = 0;
+    virtual int32_t SetParameter(const Format &params) = 0;
 };
 } // namespace Media
 } // namespace OHOS
